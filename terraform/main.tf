83f00c64ef9918f52f82d31bea5a771b4be2a2da
Josh Padnick <josh.padnick@gmail.com>
2016-09-18 12:22:07 -0700

    Upload index.html to S3.

--- terraform/main.tf
+++ terraform/main.tf
@@ -22,6 +22,22 @@ resource "aws_s3_bucket" "webform" {
     bucket = "${var.website_domain_name}"
     acl = "public-read"
 
+    # Define an S3 bucket policy that grants everyone access to all S3 objects. This way, we don't have to specify an Access
+    # Control List (ACL) for each individual S3 object to allow it to be readable by the public. 
+    policy = <<-EOF
+    {
+        "Version":"2012-10-17",
+        "Statement":[{
+            "Sid":"PublicReadGetObject",
+            "Effect":"Allow",
+            "Principal": "*",
+            "Action":["s3:GetObject"],
+            "Resource":["arn:aws:s3:::${var.website_domain_name}/*"]
+        }]
+    }
+    EOF
+
+    # Configure S3 Static Website Hosting options
     website {
         index_document = "index.html"
         error_document = "error.html" 
@@ -31,4 +47,18 @@ resource "aws_s3_bucket" "webform" {
     versioning {
       enabled = true
     }
+}
+
+# ---------------------------------------------------------------------------------------------------------------------
+# UPLOAD INDIVIDUAL PAGES TO THE S3 BUCKET
+# Note that we do not need to specify a "public-read" Access Control List (ACL) because our S3 Bucket Policy handles this
+# for us.
+# ---------------------------------------------------------------------------------------------------------------------
+
+resource "aws_s3_bucket_object" "index" {
+    bucket = "${aws_s3_bucket.webform.id}"
+    key = "index.html"
+    source = "${path.module}/web/index.html"
+    etag = "${md5(file("${path.module}/web/index.html"))}"
+    content_type = "text/html"
 }
